2963b4e339bff0b0028f9e648fe6398d280da493
carlos <carlos@2cea6d47-961a-ee4d-b3ad-84a0b2c425f3>
2008-08-21 13:50:23 +0000

    - CHG: Alterada navegação de onclick="" para <a href="">

--- Pagination.class.php
+++ Pagination.class.php
@@ -1,10 +1,6 @@
 <?
 /**
  * class Pagination
- * 
- * Babylon English-English 
- * pagination
- * n. creation of pages; division of a document into pages in a word processor (Computers)
  *
  * @version (2007/06/13)
  * @package Pagination
@@ -58,6 +54,7 @@ class Pagination{
 		if ($seo) $this->query_string = preg_replace('([&]?baseurl=true)', '', $this->query_string); // Retira a baseurl se a pagina tiver S.E.O.
 		$this->query_string = preg_replace('([&]?go_url=' . $_GET['go_url'] . ')', '', $this->query_string); // Retira a GO Url da Query String
 		$this->parameters = $parameters;
+		$this->request_uri = preg_replace('/[?](.*)/', '', $_SERVER['REQUEST_URI']);
 		//$this->query_string=substr($this->query_string,1);
 		
 		foreach ($_POST as $key=>$value) {
@@ -69,15 +66,6 @@ class Pagination{
 				// Numbers
 				$this->htm_numbers_extra = $this->htm_numbers = '<div class="numbers"><ul>';
 				
-				/* Código antigo. Exemplo: [5] 6 7 8 9 10 11 12 13 14
-				$min = $page;
-				$max = $min + $numbers_limit - 1;
-				if ($max > $this->total) {
-					$min = $this->total - $numbers_limit + 1;  	
-					$max = $this->total;
-				}
-				if ($min < 1) $min = 1;*/
-
 				$min = $page - ceil($numbers_limit / 2);  // Codigo novo. Exemplo: 1 2 3 4 [5] 6 7 8 9 10
 				$max = $min + $numbers_limit - 1; 
 				if ($min < 1) {
@@ -86,13 +74,9 @@ class Pagination{
 				}
 				if ($max > $this->total) $max = $this->total;
 						
-				//if ($page !=1 && $this->total > 2 && $page > 2) $this->htm_numbers_extra .= '<li class="' . (($page == 1) ? 'back-off' :'bgleft_plus') . '" onclick="location=\'?' . $parameters . $this->query_string . '&p_page=1\'">' . $back_char_plus . '</li>';
-				//$this->htm_numbers_extra .= '<li class="' . (($page == 1) ? 'back-off' : 'bgleft') . '" onclick="location=\'?' . $parameters . $this->query_string . '&p_page=' . ($page-1) . '\'">' . $back_char . '</li>';
 				if ($page !=1 && $this->total > 2 && $page > 2) $this->htm_numbers_extra .= $this->createLink(1, $first_char , ' class="' . (($page == 1) ? 'back-off"' :'bgleft_plus"'));
 				$this->htm_numbers_extra .= $this->createLink($page - 1, $back_char, ' class="' . (($page == 1) ? 'back-off"' :'bgleft"'));
 				for ($i = $min; $i <= $max; $i++) {
-					//$this->htm_numbers .= '<li' . (($i == $page) ? ' class="on"' : '') . ' onclick="location=\'?' . $parameters . $this->query_string . '&p_page=' . $i . '\'">' . $i . '</li>';
-					//$this->htm_numbers_extra .= '<li' . (($i == $page)? ' class="on"' : '') . ' onclick="location=\'?' . $parameters . $this->query_string . '&p_page=' . $i . '\'">' . $i . '</li>';
 					$this->htm_numbers .= $this->createLink($i, $i, ($i == $page) ? ' class="on"' : '');
 					$this->htm_numbers_extra .= $this->createLink($i, $i, ($i == $page) ? ' class="on"' : '');
 					if ($i != $max) {
@@ -100,8 +84,7 @@ class Pagination{
 						$this->htm_numbers_extra .= '<li>' . $separador . '</li>';
 					}
 				}
-				//$this->htm_numbers_extra .= '<li class="' . (($page == $this->total) ? 'go-off' : 'bgright') . '" onclick="location=\'?' . $parameters . $this->query_string . '&p_page=' . ($page + 1) . '\'">' . $go_char . '</li>';
-				//if ($page != $this->total && $this->total > 2 && $page < ($this->total - 1)) $this->htm_numbers_extra .= '<li class="' . (($page == $this->total) ? 'go-off' : 'bgright_plus') . '" onclick="location=\'?' . $parameters . $this->query_string . '&p_page=' . $this->total . '\'">' . $go_char_plus . '</li>';
+				rs . $this->query_string . '&p_page=' . $this->total . '\'">' . $go_char_plus . '</li>';
 				$this->htm_numbers_extra .= $this->createLink($page + 1, $next_char, ' class="' . (($page == $this->total) ? 'go-off"' : 'bgright"'));
 				if ($page != $this->total && $this->total > 2 && $page < ($this->total - 1)) $this->htm_numbers_extra .= $this->createLink($this->total, $last_char, ' class="' . (($page == $this->total) ? 'go-off"' : 'bgright_plus"'));
 				$this->htm_numbers_extra .= '</ul></div>';
@@ -109,12 +92,14 @@ class Pagination{
 			}
 			// Combo
 			$this->htm_combo = '<div class="text">Página</div>' .
-			'<select onchange="location=\'?' . $parameters . $this->query_string . '&p_page=\'+this[selectedIndex].value">' . "\n" .
+			'<select onchange="location=\'' . $this->request_uri  . '?' . $parameters . $this->query_string . '&p_page=\'+this[selectedIndex].value">' . "\n" .
 			'<script>jp7_num_combo(1,' . $this->total . ',' . $page . ')</script>' .
 			'</select>' . "\n" . '<div class="text">de ' . $this->total . '</div>' . "\n";
 			// Buttons
-			$this->htm_back = '<input type="button" class="back' . (($page == 1) ? ' back-off' : '') . '" onclick="location=\'?' . $parameters . $this->query_string . '&p_page=' . ($page - 1) . '\'"' . (($page == 1) ? ' disabled' : '') . '>' . "\n";
-			$this->htm_go = '<input type="button" class="go' . (($page == $this->total) ? ' go-off' : '') . '" onclick="location=\'?' . $parameters . $this->query_string . '&p_page=' . ($page + 1) . '\'"' . (($page == $this->total) ? ' disabled' : '') . '>' . "\n";
+			
+			$this->htm_back = '<input type="button" class="back' . (($page == 1) ? ' back-off' : '') . '" onclick="location=\'' . $this->request_uri . '?' . $parameters . $this->query_string . '&p_page=' . ($page - 1) . '\'"' . (($page == 1) ? ' disabled' : '') . '>' . "\n";
+			$this->htm_go = '<input type="button" class="go' . (($page == $this->total) ? ' go-off' : '') . '" onclick="location=\'' . $this->request_uri . '?' . $parameters . $this->query_string . '&p_page=' . ($page + 1) . '\'"' . (($page == $this->total) ? ' disabled' : '') . '>' . "\n";
+				
 			// Types
 			$this->htm = '<div class="jp7_db_pages" style="width:auto"><div class="' . $type . '">' . "\n";
 			if ($type == 'combo') $this->htm .= $this->htm_back . $this->htm_combo . $this->htm_go;
@@ -137,7 +122,7 @@ class Pagination{
 	 * @version (2008/06/13)
 	 */
 	private function createLink($pageNumber, $pageLabel, $className = ''){
-		return '<li' . $className . ' onclick="location=\'?' . $this->parameters . $this->query_string . (($this->parameters || $this->query_string) ? '&' : '') . 'p_page=' . $pageNumber . '\'">' . $pageLabel . '</li>';
+		return '<li' . $className . '><a href="' .  $this->request_uri . '?' . $this->parameters . $this->query_string . (($this->parameters || $this->query_string) ? '&' : '') . 'p_page=' . $pageNumber . '">' . $pageLabel . '</a></li>';
 	}
 }
 ?>
