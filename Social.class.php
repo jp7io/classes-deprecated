4237862210cfa1963971b6073af9ba6271e75972
diego <diego@2cea6d47-961a-ee4d-b3ad-84a0b2c425f3>
2008-12-02 14:33:15 +0000

    - CHG: Agora a lista de bookmarks pode ser "custom".

--- Social.class.php
+++ Social.class.php
@@ -11,8 +11,8 @@ class Social {
 		'digg' => 'Digg',
 		'live' => 'Live',
 		'reddit' => 'Reddit'
-	);
-	private $extendedSocials = array (
+		);
+		private $extendedSocials = array (
 		'twitter' => 'Twitter',
 		'linkedin' => 'LinkedIn',
 		'facebook' => 'Facebook',
@@ -23,8 +23,8 @@ class Social {
 		'multiply' => 'Multiply',
 		'technorati' => 'Technorati',
 		'yahoobkm' => 'Yahoo Bookmarks'
-	);
-	private $otherSocials = array (
+		);
+		private $otherSocials = array (
 		'propeller' => 'Propeller',
 		'backflip' => 'Backflip',
 		'kaboodle' => 'Kaboodle',
@@ -46,127 +46,130 @@ class Social {
 		'furl' => 'Furl',
 		'newsvine' => 'Newsvine',
 		'yardbarker' => 'Yardbarker'
-	);
-	private $allSocials = array ();
+		);
+		private $allSocials = array ();
 
-	private function createSocial ($theme) {
-		// Merge all socials
-		$this->allSocials = array_merge($this->mainSocials, $this->extendedSocials);
-		$this->allSocials = array_merge($this->allSocials, $this->otherSocials);
+		private function createSocial ($theme) {
+			// Merge all socials
+			$this->allSocials = array_merge($this->mainSocials, $this->extendedSocials);
+			$this->allSocials = array_merge($this->allSocials, $this->otherSocials);
 
-		switch ($theme) {
-			case 'main':
-				$this->displaySocials = $this->mainSocials;
-				break;
-			case 'extended':
-				$this->displaySocials = array_merge($this->mainSocials, $this->extendedSocials);
-				break;
-			case 'all':
-				$this->displaySocials = $this->allSocials;
-				break;
-			default:
-				$this->displaySocials = $this->mainSocials;
-				break;
-		}
-	}
-	private function addSocials ($add) {
-		// Add socials
-		foreach ($add as $item) {
-			if (array_key_exists($item, $this->allSocials)) {
-				$this->displaySocials[$item] = $this->allSocials[$item];
-			} else {
-				exit ('This social does not exist.');
+			switch ($theme) {
+				case 'main':
+					$this->displaySocials = $this->mainSocials;
+					break;
+				case 'extended':
+					$this->displaySocials = array_merge($this->mainSocials, $this->extendedSocials);
+					break;
+				case 'all':
+					$this->displaySocials = $this->allSocials;
+					break;
+				case 'custom':
+					$this->displaySocials = array ();
+					break;
+				default:
+					$this->displaySocials = $this->mainSocials;
+					break;
 			}
 		}
-	}
-	private function removeSocials ($remove) {
-		// Remove socials
-		foreach ($remove as $item) {
-			if (array_key_exists($item, $this->allSocials)) {
-				unset($this->displaySocials[$item]);
-			} else {
-				exit ('This social does not exist.');
+		private function addSocials ($add) {
+			// Add socials
+			foreach ($add as $item) {
+				if (array_key_exists($item, $this->allSocials)) {
+					$this->displaySocials[$item] = $this->allSocials[$item];
+				} else {
+					exit ('This social does not exist.');
+				}
 			}
 		}
-	}
-
-	public function newSocialBookmark ($theme = 'main', $add = array(), $remove = array()) {
-		// Create a social
-		Social::createSocial ($theme);
-
-		// Add socials if passed
-		if ($add) {
-			Social::addSocials ($add);
-		}
-
-		// Remove socials if passed
-		if ($remove) {
-			Social::removeSocials ($remove);
-		}
-	}
-	public function displayBookmark ($limiter = 5/*, $url = FALSE, $title = '', $target = '_blank'*/) {
-		// Must create a social first
-		if (!$this->displaySocials) {
-			exit('You must creat a social first.');
-		}
-
-		// Set the default configuration
-		if (!$url) {
-			$url = $_SERVER['HTTP_REFERER'];
-		}
-		
-		/*
-		$html .= '<script type="text/javascript">' . "\n";
-		$html .= 'function sets(val) {' . "\n";
-		$html .= 'elt = document.getElementById(\'bookmarkingMys\');' . "\n";
-		$html .= 'elt.value = val;' . "\n";
-		$html .= 'elt = document.getElementById(\'bookmarkingWinname\');' . "\n";
-		$html .= 'elt.value = window.name;' . "\n";
-		$html .= 'elt = document.getElementById(\'bookmarkingForm\');' . "\n";
-		$html .= 'elt.submit();' . "\n";
-		$html .= '}' . "\n";
-		$html .= '</script>' . "\n";
-
-		$html .= '<form id="bookmarkingForm" action="http://www.addthis.com/bookmark.php" method="post" target="' . $target . '">' . "\n";
-		$html .= '<input type="hidden" id="bookmarkingAte" name="ate" value="AT-internal/-/-/-/-/-" />' . "\n";
-		$html .= '<input type="hidden" id="bookmarkingMys" name="s" value="" />' . "\n";
-		$html .= '<input type="hidden" id="bookmarkingPub" name="pub" value="" />' . "\n";
-		$html .= '<input type="hidden" id="bookmarkingUrl" name="url" value="' . $url . '" />' . "\n";
-		$html .= '<input type="hidden" id="bookmarkingTitle" name="title" value="' . $title . '" />' . "\n";
-		$html .= '<input type="hidden" id="bookmarkingLng" name="lng" value="" />' . "\n";
-		$html .= '<input type="hidden" id="bookmarkingWinname" name="winname" value="" />' . "\n";
-		$html .= '<input type="hidden" id="bookmarkingContent" name="content" value="" />' . "\n";
-		$html .= '</form>' . "\n";
-		*/
-
-		$html = '<div id="bookmark">' . "\n";
-		$html .= '<ul>' . "\n";
-		$count = 1;
-		foreach($this->displaySocials as $key=>$value) {
-			#$html .= '<li><a href="javascript:sets(\'' . $key . '\');"><span class="at15t at15t_' . $key . '">' . $value . '</span></a></li>' . "\n";
-			$html .= '<li><a href="javascript:void(0);" onclick="return addthis_sendto(\'' . $key . '\');"><span class="at15t at15t_' . $key . '">' . $value . '</span></a></li>' . "\n";
-				
-			if($count == $limiter) {
-				$html .= '</ul>' . "\n" . '<ul>' . "\n";
-				$count = 0;
+		private function removeSocials ($remove) {
+			// Remove socials
+			foreach ($remove as $item) {
+				if (array_key_exists($item, $this->allSocials)) {
+					unset($this->displaySocials[$item]);
+				} else {
+					exit ('This social does not exist.');
+				}
 			}
-			$count++;
 		}
-		$html .= '</ul>' . "\n";
-		$html .= '</div>' . "\n";
 
-		return $html;
-	}
-	/* Bookmarking */
+		public function newSocialBookmark ($theme = 'main', $add = array (), $remove = array ()) {
+			// Create a social
+			Social::createSocial ($theme);
 
-	/* Send to a friend */
-	public function newSendFriend ($url = FALSE, $title = '', $action = '../indique/enviar.php', $target = '_parent', $template = array (
+			// Add socials if passed
+			if ($add) {
+				Social::addSocials ($add);
+			}
+			
+			// Remove socials if passed
+			if ($remove) {
+				Social::removeSocials ($remove);
+			}
+		}
+		public function displayBookmark ($limiter = 5/*, $url = FALSE, $title = '', $target = '_blank'*/) {
+			// Must create a social first
+			if (!$this->displaySocials) {
+				exit('You must creat a social first.');
+			}
+
+			// Set the default configuration
+			if (!$url) {
+				$url = $_SERVER['HTTP_REFERER'];
+			}
+
+			/*
+			 $html .= '<script type="text/javascript">' . "\n";
+			 $html .= 'function sets(val) {' . "\n";
+			 $html .= 'elt = document.getElementById(\'bookmarkingMys\');' . "\n";
+			 $html .= 'elt.value = val;' . "\n";
+			 $html .= 'elt = document.getElementById(\'bookmarkingWinname\');' . "\n";
+			 $html .= 'elt.value = window.name;' . "\n";
+			 $html .= 'elt = document.getElementById(\'bookmarkingForm\');' . "\n";
+			 $html .= 'elt.submit();' . "\n";
+			 $html .= '}' . "\n";
+			 $html .= '</script>' . "\n";
+
+			 $html .= '<form id="bookmarkingForm" action="http://www.addthis.com/bookmark.php" method="post" target="' . $target . '">' . "\n";
+			 $html .= '<input type="hidden" id="bookmarkingAte" name="ate" value="AT-internal/-/-/-/-/-" />' . "\n";
+			 $html .= '<input type="hidden" id="bookmarkingMys" name="s" value="" />' . "\n";
+			 $html .= '<input type="hidden" id="bookmarkingPub" name="pub" value="" />' . "\n";
+			 $html .= '<input type="hidden" id="bookmarkingUrl" name="url" value="' . $url . '" />' . "\n";
+			 $html .= '<input type="hidden" id="bookmarkingTitle" name="title" value="' . $title . '" />' . "\n";
+			 $html .= '<input type="hidden" id="bookmarkingLng" name="lng" value="" />' . "\n";
+			 $html .= '<input type="hidden" id="bookmarkingWinname" name="winname" value="" />' . "\n";
+			 $html .= '<input type="hidden" id="bookmarkingContent" name="content" value="" />' . "\n";
+			 $html .= '</form>' . "\n";
+			 */
+
+			$html = '<div id="bookmark">' . "\n";
+			$html .= '<ul>' . "\n";
+			$count = 1;
+			foreach($this->displaySocials as $key=>$value) {
+				#$html .= '<li><a href="javascript:sets(\'' . $key . '\');"><span class="at15t at15t_' . $key . '">' . $value . '</span></a></li>' . "\n";
+				$html .= '<li><a href="javascript:void(0);" onclick="return addthis_sendto(\'' . $key . '\');"><span class="at15t at15t_' . $key . '">' . $value . '</span></a></li>' . "\n";
+
+				if($count == $limiter) {
+					$html .= '</ul>' . "\n" . '<ul>' . "\n";
+					$count = 0;
+				}
+				$count++;
+			}
+			$html .= '</ul>' . "\n";
+			$html .= '</div>' . "\n";
+
+			return $html;
+		}
+		/* Bookmarking */
+
+		/* Send to a friend */
+		public function newSendFriend ($url = FALSE, $title = '', $action = '../indique/enviar.php', $target = '_parent', $template = array (
 		'form' => 'global',
 		'mail' => 'global',
 		'success' => 'global',
 		'fail' => 'global'
-	),
-	$messages = array (
+		),
+		$messages = array (
 		'legend' => 'Envie para um amigo',
 		'yourName' => 'Seu nome:',
 		'yourMail' => 'Seu e-mail:',
@@ -174,34 +177,72 @@ class Social {
 		'friendMailLabel' => '(separe os e-mails com vírgula)',
 		'friendComment' => 'Comentário:',
 		'send' => 'Enviar'
-	)) {
-		#global $c_site, $c_url, $c_cliente_title;
-		// Set the default configuration
-		if (!$url) {
-			$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-		}
-		if ($template['form'] == 'global' or $template == false or $template['form'] == false) {
-			$template['form'] = jp7_path_find('../../_default/site/_templates/sendFriend/form.htm');
-		} else {
-			$template['form'] = jp7_path_find('../site/_templates/' . $template['form']);
-		}
+		)) {
+			// Set the default configuration
+			if (!$url) {
+				$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+			}
+			if ($template['form'] == 'global' or $template == false or $template['form'] == false) {
+				$template['form'] = jp7_path_find('../../_default/site/_templates/sendFriend/form.htm');
+			} else {
+				$template['form'] = jp7_path_find('../site/_templates/' . $template['form']);
+			}
 
-		$html = file_get_contents($template['form']);
-		$html = str_replace('%action%', $action, $html);
-		$html = str_replace('%target%', $target, $html);
-		$html = str_replace('%template%', $template['mail'], $html);
-		$html = str_replace('%success%', $template['success'], $html);
-		$html = str_replace('%fail%', $template['fail'], $html);
-		$html = str_replace('%url%', $url, $html);
-		$html = str_replace('%title%', $title, $html);
-		$html = str_replace('%yourName%', $messages['yourName'], $html);
-		$html = str_replace('%yourMail%', $messages['yourMail'], $html);
-		$html = str_replace('%friendMail%', $messages['friendMail'], $html);
-		$html = str_replace('%friendMailLabel%', $messages['friendMailLabel'], $html);
-		$html = str_replace('%friendComment%', $messages['friendComment'], $html);
-		$html = str_replace('%send%', $messages['send'], $html);
+			$html = file_get_contents($template['form']);
+			$html = str_replace('%action%', $action, $html);
+			$html = str_replace('%target%', $target, $html);
+			$html = str_replace('%template%', $template['mail'], $html);
+			$html = str_replace('%success%', $template['success'], $html);
+			$html = str_replace('%fail%', $template['fail'], $html);
+			$html = str_replace('%url%', $url, $html);
+			$html = str_replace('%title%', $title, $html);
+			$html = str_replace('%yourName%', $messages['yourName'], $html);
+			$html = str_replace('%yourMail%', $messages['yourMail'], $html);
+			$html = str_replace('%friendMail%', $messages['friendMail'], $html);
+			$html = str_replace('%friendMailLabel%', $messages['friendMailLabel'], $html);
+			$html = str_replace('%friendComment%', $messages['friendComment'], $html);
+			$html = str_replace('%send%', $messages['send'], $html);
 
-		return $html;
-	}
-	/* Send to a friend */
+			return $html;
+		}
+		/* Send to a friend */
+
+		/* Embedeed */
+		/*
+		 * http://www.addthis.com/bookmark.php?
+		 * v=15&
+		 * winname=addthis&
+		 * pub=&
+		 * s=google&
+		 * url=&
+		 * title=&
+		 * logo=&
+		 * logobg=&
+		 * logocolor=&
+		 * ate=AT-unknown/-/-/7ef9a70aa725f8/3/491c825d27b8595b&
+		 * adt=undefined&
+		 * content=&
+		 * CXNID=2000001.5215456080540439074NXC
+		 *
+		 */
+		public function displayEmbedded($title = 'Clique aqui', $url = false, $type = 'link', $label = 'Endereço:') {
+			if (!$url) {
+				$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+			}
+
+			$html = '<div id="embedded">' . "\n";
+			$html .= '<label for="embeddedUrl' . ucfirst(strtolower($key)) . '">' . $label . '</label><br />' . "\n";
+			$html .= '<textarea id="embeddedUrl' . ucfirst(strtolower($key)) . '" type="text" readonly="readonly" onclick="javascript:document.getElementById(\'embeddedUrl' . ucfirst(strtolower($key)) . '\').focus();document.getElementById(\'embeddedUrl' . ucfirst(strtolower($key)) . '\').select();">'  . "\n";
+			if ($type == 'link') {
+				$html .= '<a href="' . $url . '" target="_blank">' . $title . '</a>';
+			} else {
+				exit('This type is not implemented yet');
+			}
+			$html .= '</textarea>'  . "\n";
+			$html .= '<div style="clear:both;"></div>';
+			$html .= '</div>' . "\n";
+
+			return $html;
+		}
+		/* Embedeed */
 }
