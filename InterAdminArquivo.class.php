12d48a9dcc869df31822c2bd16d90a0265578a9d
carlos <carlos@2cea6d47-961a-ee4d-b3ad-84a0b2c425f3>
2009-01-12 14:37:12 +0000

    - BUG: Correção do getFieldsValues. - NEW: Adicionado getUrl(). - DEV: Comentários PhpDOC.

--- InterAdminArquivo.class.php
+++ InterAdminArquivo.class.php
@@ -15,22 +15,53 @@
  * @package InterAdminArquivo
  */
 class InterAdminArquivo{
+	/**
+	 * This record's 'id_arquivo', which is its primary key.
+	 * @var int
+	 */	
 	public $id_arquivo;
+	/**
+	 * This record's 'id', which is the parent of this InterAdminArquivo.
+	 * @var int
+	 */
 	public $id;
+	/**
+	 * This record's 'id_tipo', this value can be used to get the InterAdminTipo for a InterAdmin object.
+	 * @var int
+	 */
 	public $id_tipo;
+	/**
+	 * Table prefix of this record. It is usually formed by 'interadmin_' + 'client name'.
+	 * @var string
+	 */
 	public $db_prefix;
+	/**
+	 * Contains the InterAdminTipo, i.e. the record with an 'id_tipo' equal to this record´s 'id_tipo'.
+	 * @var InterAdminTipo
+	 */
 	protected $_tipo;
+	/**
+	 * Contains the parent InterAdmin object, i.e. the record with an 'id' equal to this record's 'parent_id'.
+	 * @var InterAdmin
+	 */
 	protected $_parent;
 	/**
-	 * @param int $id
-	 * @param array $options
+	 * Public Constructor. If $options['fields'] was passed the method $this->getFieldsValues() is called.
+	 * 
+	 * @param int $id_arquivo This record's 'id_arquivo'.
+	 * @param array $options Default array of options. Available keys: db_prefix, fields.
 	 */
-	function __construct($id_arquivo = 0, $options = array()) {
+	public function __construct($id_arquivo = 0, $options = array()) {
 		$this->id_arquivo = $id_arquivo;
 		$this->db_prefix = ($options['db_prefix']) ? $options['db_prefix'] : $GLOBALS['db_prefix'];
 		if ($options['fields']) $this->getFieldsValues($options['fields']);
 	}
-	function __toString(){
+	/**
+	 * String value of this record´s $id_arquivo.
+	 * 
+	 * @return string String value of the $id_arquivo property.
+	 */
+	public function __toString(){
 		return (string) $this->id_arquivo;
 	}	
 	/**
@@ -40,22 +71,31 @@ class InterAdminArquivo{
 	 * @return mixed If fields were an array an object will be returned, otherwise it will return the result as a string.
 	 */
 	function getFieldsValues($fields) {   
-		global $lang;
+		global $lang, $db;
 		if (!$this->_tipo) $this->getTipo();
-		if ($this->_tipo->id_tipo) $campos = $this->_tipo->getCampos();
-		if ($this->_tipo->id_tipo && $lang->prefix) $tipoLanguage = $this->_tipo->getFieldsValues('language');
-		
-		$fieldsValues = jp7_fields_values($this->db_prefix . (($tipoLanguage) ? $lang->prefix : '') . '_arquivos', 'id_arquivo', $this->id_arquivo, $fields, TRUE);
-		foreach ((array) $fieldsValues as $field=>$value) {
-			$this->$field = $value;
+		if ($lang->prefix) $tipoLanguage = $this->_tipo->getFieldsValues('language');
+		if ($fields == '*') $fields = InterAdminArquivo::getAllFieldsNames();
+		 
+		$sql = "SELECT " . implode(',' , (array) $fields) . 
+			" FROM " . $this->db_prefix . (($tipoLanguage) ? $lang->prefix : '') . "_arquivos" .
+			" WHERE id_arquivo = " . $this->id_arquivo;
+		$rs = $db->Execute($sql) or die(jp7_debug($db->ErrorMsg(), $sql));
+		if ($fieldsValues = $rs->FetchNextObj()) {
+			foreach ((array) $fieldsValues as $field=>$value) {
+				$this->$field = $value;
+			}
+			if (is_array($fields)) return $fieldsValues;
+			else return $fieldsValues->$fields;
 		}
-		if (is_array($fields)) return $fieldsValues;
-		else return $fieldsValues->$fields;
 	}
 	/**
-	 * @return mixed
+	 * Updates the values into the database table. If this object has no 'id_arquivo', the data is inserted.
+	 * 
+	 * @param array $fields_values Array with the values, the keys are the fields names.
+	 * @param bool $force_magic_quotes_gpc If TRUE the string will be quoted even if 'magic_quotes_gpc' is not active. 
+	 * @return void
 	 */
-	function setFieldsValues($fields_values, $force_magic_quotes_gpc = FALSE) {
+	public function setFieldsValues($fields_values, $force_magic_quotes_gpc = FALSE) {
 		global $lang;
 		$tipoLanguage = $this->getTipo()->getFieldsValues('language');
 		if ($this->id_arquivo) {
@@ -65,33 +105,69 @@ class InterAdminArquivo{
 		}
 	}
 	/**
-	 * @return object
+	 * Returns an array with the names of all the fields.
+	 * 
+	 * @return array
 	 */
-	function getTipo() {
-		if (!$this->_tipo) $this->_tipo = new InterAdminTipo($this->id_tipo, array('db_prefix' => $this->db_prefix));
-		if (!$this->id_tipo) $this->_tipo->id_tipo = $this->id_tipo = $this->getFieldsValues('id_tipo');
+	public static function getAllFieldsNames(){
+		return array('id_arquivo', 'id_tipo', 'id', 'parte', 'url', 'url_thumb', 'url_zoom', 'url_mac', 'nome', 'legenda', 'creditos', 'link', 'link_blank', 'mostrar', 'destaque', 'ordem', 'deleted');
+	}
+	
+	/**
+	 * Gets the InterAdminTipo object for this record, which is then cached on the $_tipo property.
+	 * 
+	 * @param array $options Default array of options. Available keys: class.
+	 * @return InterAdminTipo
+	 */
+	public function getTipo($options = array()) {
+		if (!$this->_tipo) {
+			if (!$this->id_tipo) $this->id_tipo = jp7_fields_values($this->db_prefix . $this->table, 'id', $this->id, 'id_tipo');
+			$this->_tipo = InterAdminTipo::getInstance($this->id_tipo, array('db_prefix' => $this->db_prefix, 'class' => $options['class']));
+		}
 		return $this->_tipo;
 	}
 	/**
+	 * Sets the InterAdminTipo object for this record, changing the $_tipo property.
 	 *
+	 * @param InterAdminTipo $tipo
+	 * @return void
 	 */
-	function setTipo($tipo) {
+	public function setTipo($tipo) {
 		$this->id_tipo = $tipo->id_tipo;
 		$this->_tipo = $tipo;
 	}
 	/**
+	 * Gets the parent InterAdmin object for this record, which is then cached on the $_parent property.
+	 * 
+	 * @param array $options Default array of options. Available keys: db_prefix, table, fields, fields_alias, class.
 	 * @return InterAdmin
 	 */
-	function getParent() {
+	public function getParent($options = array()) {
 		if ($this->_parent) return $this->_parent;
-		$id = $this->getFieldsValues(array('id'))->id;
-		if ($id) {
-			$this->_parent = new InterAdmin($id, array('db_prefix' => $this->db_prefix));
-			return $this->_parent;
+		if ($this->id || $this->getFieldsValues('id')) {
+			return $this->_parent = InterAdmin::getInstance($this->id, $options);
 		}
 	}
-	function setParent($parent) {
+	/**
+	 * Sets the parent InterAdmin object for this record, changing the $_parent property.
+	 *
+	 * @param InterAdmin $parent
+	 * @return void
+	 */
+	public function setParent($parent) {
+		$this->id = $parent->id;
 		$this->_parent = $parent;
+	}
+	/**
+	 * Returns the full url address of this file.
+	 *
+	 * @return string
+	 */
+	public function getUrl(){
+		global $c_url;
+		$url = ($this->url) ? $this->url : $this->getFieldsValues('url');
+		$url = str_replace('../../', $c_url, $url);
+		return $url; 
 	}
 }
 ?>
