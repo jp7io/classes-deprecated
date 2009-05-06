164e2afb33f744c1fcb04274746f2dac685c40c3
jp <jp@2cea6d47-961a-ee4d-b3ad-84a0b2c425f3>
2009-05-06 14:16:01 +0000

    - NEW: Primeira versão do classes no repositório. - CHG: Classes criadas e/ou alteradas para funcionamento MVC (site B2W).

---
+++ tags/release-1.5/Jp7/Locale.php
@@ -0,0 +1,22 @@
+<?php
+
+class Jp7_Locale
+{
+	public $lang = '', $prefix = '', $path = '';
+
+	public function __construct($language)
+	{
+		$c_default_lang = 'pt-br';
+
+		if (!in_array($language, array('de', 'en', 'es', 'fr', 'jp', 'pt', 'pt-br'))) {
+			$language = $c_default_lang;
+		}
+
+		$this->lang = $language;
+		$this->path = '/' . $language . '/';
+
+		if ($language != 'pt-br') {
+			$this->prefix = '_' . $language;
+		}
+	}
+}
