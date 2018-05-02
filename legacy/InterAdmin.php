a522cf8b2cc4ce77a16fa58b3f314a0c4bc3e973
Diego Garcia <12803744+Dugo03@users.noreply.github.com>
2018-05-02 15:29:09 -0300

    Fix query for varchar_key when it's not used

--- legacy/InterAdmin.php
+++ legacy/InterAdmin.php
@@ -402,7 +402,7 @@ class InterAdmin extends Record implements InterAdminAbstract
     public function save()
     {
         // Using id_string is deprecated, use id_slug instead
-        if (empty($this->attributes['id_string']) && in_array('id_string', $this->getColumns())) {
+        if (empty($this->attributes['id_string']) && array_key_exists('varchar_key', $this->_aliases) && in_array('id_string', $this->getColumns())) {
             $this->id_string = toId($this->varchar_key);
         }
         return parent::save();
